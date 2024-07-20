1d06156454d864631fd675891eb578aae3c38a98
Rushikesh Patil <prushikeh@gmail.com>
2024-07-20 16:30:33 +0530

    First Commit

---
+++ src/components/Block.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css'
+
+interface BlockProps {
+    value?: String | null;
+    onClick?: () => void
+}
+
+const Block: React.FC<BlockProps> = (props) => {
+    return (
+        <div onClick={props.onClick} className='block'>
+            {props.value}
+        </div>
+    )
+};
+
+export default Block;
